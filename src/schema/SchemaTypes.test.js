c6238cd54e27020f364143168b6a19458500629f
Dan Levy <dan@danlevy.net>
2016-10-25 21:25:57 -0600

    broken guessType(30) ... fixing

--- src/schema/SchemaTypes.test.js
+++ src/schema/SchemaTypes.test.js
@@ -1,17 +1,18 @@
-require('babel-register');
+// require('babel-register');
 
-const test   = require('tape');
+const test   = require('ava');
 const {guessType} = require('./SchemaTypes');
 
 test('guessType type checker', t => {
-  t.equal(guessType(new Set()).type, 'object')
-  t.equal(guessType(Array(1)).type, 'array')
-  t.equal(guessType('[]').type, 'string')
-  t.equal(guessType('["test"]').type, 'string')
-  t.equal(guessType({}).type, 'object')
-  t.equal(guessType('{}').type, 'string')
-  t.equal(guessType('{"foo":"bar"}').type, 'string')
-  t.equal(guessType('true').type, 'boolean')
-  t.equal(guessType('YES').type, 'boolean')
+  t.is(guessType(new Set()).type, 'object')
+  t.is(guessType(Array(1)).type, 'array')
+  t.is(guessType('[]').type, 'string')
+  t.is(guessType('["test"]').type, 'string')
+  t.is(guessType({}).type, 'object')
+  t.is(guessType('{}').type, 'string')
+  t.is(guessType('{"foo":"bar"}').type, 'string')
+  t.is(guessType('true').type, 'boolean')
+  t.is(guessType('YES').type, 'boolean')
+  t.is(guessType(30).type, 'number')
   t.end();
 })
